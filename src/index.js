d9c69b173017bd5e2255d9325c4add82b759c6da
Patrick Wauschek <patrick@patrickwcode.com>
2022-02-25 04:12:32 -0600

    Adds endpoint for searching appId by name

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 const express = require('express')
-const app = express()
 const port = 3000
 let steamWebApiKey;
 
 // Read and get Steam Web API key from JSON file.
 const fs = require('fs')
-try {
-    const data = fs.readFileSync("../steam-web-api-key.json", "utf8")
-    dataJSON = JSON.parse(data).steamWebApiKey
-    steamWebApiKey = dataJSON
-} catch (err) {
-    console.error(err)
-}
+const data = fs.readFileSync("steam-web-api-key.json", "utf8")
+dataJSON = JSON.parse(data).steamWebApiKey
+steamWebApiKey = dataJSON
+
 const SteamAPI = require('steamapi')
 const steam = new SteamAPI(steamWebApiKey)
+const app = express()
+let cachedAppList = {}
 
 getGameAchievements = async (id) => {
     let achievements = await steam.getGameAchievements(id)
@@ -29,15 +27,14 @@ getGameSchema = async (id) => {
 
 getAppList = async () => {
     let appList = await steam.getAppList()
-    let sortedAppList = {}
+    let appListToLowerCase = {}
     appList.forEach(app => {
-        const appIdString = `${app.appid}`
-        sortedAppList[app.name.toLowerCase()] = {
-            appId: appIdString,
-            appName: app.name
+        appListToLowerCase[app.name.toLowerCase()] = {
+            appid: app.appid,
+            name: app.name
         }
     })
-    return sortedAppList
+    return appListToLowerCase
 }
 
 getSteamAchievements = async (id) => {
@@ -62,11 +59,21 @@ getSteamAchievements = async (id) => {
 }
 
 app.get('/achievements', async (req, res) => {
-    res.send(await getSteamAchievements(req.query.id))
+    res.send(await getSteamAchievements(req.query.id));
 })
 
 app.get('/applist', async (req, res) => {
     res.send(await getAppList())
 })
 
-app.listen(10000)
+app.get('/app', (req, res) => {
+    res.send(cachedAppList[req.query.name])
+})
+
+init = async () => {
+    cachedAppList = await getAppList()
+    app.listen (10000)
+}
+
+init()
+
