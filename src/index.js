992394150a18126da7bc59f58a660dada55efcde
Patrick Wauschek <patrick@patrickwcode.com>
2022-03-15 12:04:53 -0500

    Limits filtered apps to 60. Previous commit does not work.

--- src/index.js
+++ src/index.js
@@ -7,180 +7,180 @@ const app = express();
 const server = app.listen();
 let cachedAppList = {};
 const emojiRegex =
-    /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}]/gu;
+  /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}]/gu;
 const idRegex = /^[0-9]+$/;
 
 getGameAchievements = async (id) => {
-    return await steam.getGameAchievements(id);
+  return await steam.getGameAchievements(id);
 };
 
 getGameSchema = async (id) => {
-    let schema = await steam.getGameSchema(id);
-    let availableGameStats = schema["availableGameStats"];
-    return availableGameStats["achievements"];
+  let schema = await steam.getGameSchema(id);
+  let availableGameStats = schema["availableGameStats"];
+  return availableGameStats["achievements"];
 };
 
 getAppList = async () => {
-    let appList = await steam.getAppList();
-    let appListToLowerCase = {};
-    appList.forEach((app) => {
-        appListToLowerCase[app.name.toLowerCase()] = {
-            appid: app.appid,
-            name: app.name,
-        };
-    });
-    return appListToLowerCase;
+  let appList = await steam.getAppList();
+  let appListToLowerCase = {};
+  appList.forEach((app) => {
+    appListToLowerCase[app.name.toLowerCase()] = {
+      appid: app.appid,
+      name: app.name,
+    };
+  });
+  return appListToLowerCase;
 };
 
 getSteamAchievementsById = async (id) => {
-    const gameAchievements = await getGameAchievements(id);
-    const gameSchemaArray = await getGameSchema(id);
-    let steamAchievements = [];
-    let counter = 0;
+  const gameAchievements = await getGameAchievements(id);
+  const gameSchemaArray = await getGameSchema(id);
+  let steamAchievements = [];
+  let counter = 0;
 
-    gameSchemaArray.forEach((gameSchema) => {
-        const percent = gameAchievements[gameSchema.name];
-        const achievement = {
-            id: counter + 1,
-            name: gameSchema.displayName,
-            percent: percent,
-            description: gameSchema.description,
-            iconUrl: gameSchema.icon,
-        };
-        steamAchievements.push(achievement);
-        counter++;
-    });
+  gameSchemaArray.forEach((gameSchema) => {
+    const percent = gameAchievements[gameSchema.name];
+    const achievement = {
+      id: counter + 1,
+      name: gameSchema.displayName,
+      percent: percent,
+      description: gameSchema.description,
+      iconUrl: gameSchema.icon,
+    };
+    steamAchievements.push(achievement);
+    counter++;
+  });
 
-    return steamAchievements;
+  return steamAchievements;
 };
 
 getSteamAchievementsByName = async (name) => {
-    if (isNullOrEmpty(name)) {
-        console.error(`Error: [${name}]`);
-        return;
-    }
-    let cachedName = name.toLowerCase();
-    const cachedApp = cachedAppList[cachedName];
-    if (isNullOrEmpty(cachedApp)) {
-        console.error(`Invalid app name: [${name}]`);
-        return;
-    }
-    let appId = cachedAppList[name].appid;
-    return await getSteamAchievementsById(appId);
+  if (isNullOrEmpty(name)) {
+    console.error(`Error: [${name}]`);
+    return;
+  }
+  let cachedName = name.toLowerCase();
+  const cachedApp = cachedAppList[cachedName];
+  if (isNullOrEmpty(cachedApp)) {
+    console.error(`Invalid app name: [${name}]`);
+    return;
+  }
+  let appId = cachedAppList[name].appid;
+  return await getSteamAchievementsById(appId);
 };
 
 const isNullOrEmpty = (str) => {
-    return str === undefined || str === null || str === "";
+  return str === undefined || str === null || str === "";
 };
 
 const isIdValid = (id) => {
-    if (
-        id > 0 &&
-        id !== " " &&
-        id !== "%20" &&
-        idRegex.test(id) &&
-        !emojiRegex.test(id)
-    ) {
-        return true;
-    }
+  if (
+    id > 0 &&
+    id !== " " &&
+    id !== "%20" &&
+    idRegex.test(id) &&
+    !emojiRegex.test(id)
+  ) {
+    return true;
+  }
 };
 
 const isNameValid = (name) => {
-    if (name !== " " && name !== "%20" && !emojiRegex.test(name)) {
-        return true;
-    }
+  if (name !== " " && name !== "%20" && !emojiRegex.test(name)) {
+    return true;
+  }
 };
 
 app.get("/achievements", async (req, res) => {
-    const id = req.query.id;
-    const name = req.query.name;
-    if (isNullOrEmpty(id) && isNullOrEmpty(name)) {
-        res
-            .status(400)
-            .send(
-                "Bad Request. Either the 'id' or 'name' query parameter must be set."
-            );
+  const id = req.query.id;
+  const name = req.query.name;
+  if (isNullOrEmpty(id) && isNullOrEmpty(name)) {
+    res
+      .status(400)
+      .send(
+        "Bad Request. Either the 'id' or 'name' query parameter must be set."
+      );
+    return;
+  }
+  if (!isIdValid(id) && !isNameValid(name)) {
+    res
+      .status(400)
+      .send(
+        "Bad Request. Either the 'id' or 'name' query parameter must be set."
+      );
+    return;
+  }
+  try {
+    if (id) {
+      res.send(await getSteamAchievementsById(id));
+      return;
+    }
+    if (name) {
+      if (!cachedAppList[name]) {
+        res.status(404).send(`Error 404. [${name}] not found.`);
         return;
+      }
+      res.send(await getSteamAchievementsByName(name));
+      return;
     }
-    if (!isIdValid(id) && !isNameValid(name)) {
-        res
-            .status(400)
-            .send(
-                "Bad Request. Either the 'id' or 'name' query parameter must be set."
-            );
-        return;
-    }
-    try {
-        if (id) {
-            res.send(await getSteamAchievementsById(id));
-            return;
-        }
-        if (name) {
-            if (!cachedAppList[name]) {
-                res.status(404).send(`Error 404. [${name}] not found.`);
-                return;
-            }
-            res.send(await getSteamAchievementsByName(name));
-            return;
-        }
-    } catch (err) {
-        res.status(404).send(err + `. Error 404. Not found.`);
-        console.error(err);
-    }
-    res.status(500).send("Internal Server Error.");
+  } catch (err) {
+    res.status(404).send(err + `. Error 404. Not found.`);
+    console.error(err);
+  }
+  res.status(500).send("Internal Server Error.");
 });
 
 app.get("/applist", (req, res) => {
-    const name = req.query.name;
-    if (isNullOrEmpty(name)) {
-        res.status(400).send("Bad Request. 'name' query parameter must be set.");
-        return;
-    }
-    if (!isNameValid(name)) {
-        res.status(400).send("Bad Request. 'name' query parameter must be set.");
-        return;
-    }
-    if (!cachedAppList[name]) {
-        res.status(404).send(`Error 404. [${name}] app not found.`);
-        return;
-    } else {
-        res.send(cachedAppList[name]);
-    }
-    res.status(500).send("Server error.");
+  const name = req.query.name;
+  if (isNullOrEmpty(name)) {
+    res.status(400).send("Bad Request. 'name' query parameter must be set.");
+    return;
+  }
+  if (!isNameValid(name)) {
+    res.status(400).send("Bad Request. 'name' query parameter must be set.");
+    return;
+  }
+  if (!cachedAppList[name]) {
+    res.status(404).send(`Error 404. [${name}] app not found.`);
+    return;
+  } else {
+    res.send(cachedAppList[name]);
+  }
+  res.status(500).send("Server error.");
 });
 
 app.get("/applist-filter", (req, res) => {
-    let counter = 0;
-    const name = req.query.name;
-    if (isNullOrEmpty(name)) {
-        res.status(400).send("Bad Request. 'name' query parameter must be set.");
-        return;
-    }
-    if (!isNameValid(name)) {
-        res.status(400).send("Bad Request. 'name' query parameter must be set.");
-        return;
-    }
-    const filteredApps = Object.keys(cachedAppList)
-        .filter((key) => key.includes(name))
-        .reduce((apps, key) => {
-            if (counter <= 60) {
-                apps[key] = cachedAppList[key];
-                counter++;
-                return apps;
-            }
-        }, {});
-    // res.send(Object.keys(filteredApps).splice(1, 30));
-    res.send(filteredApps);
-    res.status(500).send("Server error.");
+  let counter = 0;
+  const name = req.query.name;
+  if (isNullOrEmpty(name)) {
+    res.status(400).send("Bad Request. 'name' query parameter must be set.");
+    return;
+  }
+  if (!isNameValid(name)) {
+    res.status(400).send("Bad Request. 'name' query parameter must be set.");
+    return;
+  }
+  const filteredApps = Object.keys(cachedAppList)
+    .filter((key) => key.includes(name))
+    .reduce((apps, key) => {
+      if (counter <= 60) {
+        apps[key] = cachedAppList[key];
+        counter++;
+      }
+      return apps;
+    }, {});
+  // res.send(Object.keys(filteredApps).splice(1, 30));
+  res.send(filteredApps);
+  res.status(500).send("Server error.");
 });
 
 init = async () => {
-    try {
-        cachedAppList = await getAppList();
-    } catch (err) {
-        console.error("Server error getting App List.");
-    }
-    app.listen(10000);
+  try {
+    cachedAppList = await getAppList();
+  } catch (err) {
+    console.error("Server error getting App List.");
+  }
+  app.listen(10000);
 };
 
 init();
