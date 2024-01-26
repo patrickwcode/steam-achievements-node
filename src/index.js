21f9aeee79e1ede7affdc3dd9f92a64f1b4e9a98
Patrick Wauschek <patrick@patrickwcode.com>
2024-01-26 10:55:49 -0600

    Revert "Revert "Using steam-web to see if app has game achievements""
    This reverts commit c83506510d32c89580e64e6862d7502657336359.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const process = require("process");
 const steamWebApiKey = process.env.STEAM_WEB_API_KEY;
 const SteamAPI = require("steamapi");
+const steamWeb = require("steam-web");
 const steam = new SteamAPI(steamWebApiKey);
+const steam2 = new steamWeb({
+  apiKey: steamWebApiKey,
+  format: "json", //optional ['json', 'xml', 'vdf']
+});
 const app = express();
 let cachedAppList = {};
 const idRegex = /^[\d]+$/;
@@ -86,11 +91,19 @@ app.get("/achievements", async (req, res) => {
   const id = req.query.id;
   const name = req.query.name;
   if (isNullOrEmpty(id) && isNullOrEmpty(name)) {
-    res.status(400).send("Bad Request. Either the 'id' or 'name' query parameter must be set.");
+    res
+      .status(400)
+      .send(
+        "Bad Request. Either the 'id' or 'name' query parameter must be set."
+      );
     return;
   }
   if (!isIdValid(id) && !isNameValid(name)) {
-    res.status(400).send("Bad Request. Either the 'id' or 'name' query parameter must be set.");
+    res
+      .status(400)
+      .send(
+        "Bad Request. Either the 'id' or 'name' query parameter must be set."
+      );
     return;
   }
   try {
@@ -149,7 +162,8 @@ app.get("/applist-filter", async (req, res) => {
   const filteredApps = Object.keys(cachedAppList)
     .filter((key) => key.includes(name))
     .reduce((apps, key) => {
-      if (numOfApps < 50) {
+      if (numOfApps < 100) {
+        console.log(key);
         apps[key] = cachedAppList[key];
         numOfApps++;
       }
@@ -158,20 +172,38 @@ app.get("/applist-filter", async (req, res) => {
 
   const getAppsWithPlayers = async () => {
     try {
+      console.log("Started getAppsWithPlayers");
       for (const app in filteredApps) {
-        if (await steam.getGamePlayers(filteredApps[app].appid) > 0) {
-          appsWithPlayers[app] = filteredApps[app];
-        }
+        // if (await steam.getGamePlayers(filteredApps[app].appid) > 0) {
+        //   appsWithPlayers[app] = filteredApps[app];
+        // }
+        await steam2.getGlobalAchievementPercentagesForApp({
+          gameid: filteredApps[app].appid,
+          callback: async (err, achievementsData) => {
+            try {
+              if (achievementsData !== undefined) {
+                console.log("GOT ONE HERE");
+                appsWithPlayers[app] = filteredApps[app];
+              }
+            } catch (err) {
+              console.error(err);
+
+              throw err;
+            }
+          },
+        });
       }
     } catch (err) {
+      console.log("Error in getAppsWithPlayers");
       console.error(err);
       // res.status(500).send("Server Error");
 
-      throw (err);
+      throw err;
     } finally {
+      console.log("SENDING NOW apps with players = " + appsWithPlayers);
       res.send(appsWithPlayers);
     }
-  }
+  };
 
   await getAppsWithPlayers();
 });
@@ -191,8 +223,8 @@ const init = async () => {
   } catch (err) {
     console.error("Server error getting App List.");
   }
-  app.listen(10000, '127.0.0.1', function () {
-    console.log("... port %d in %s mode", 10000, '127.0.0.1');
+  app.listen(10000, "127.0.0.1", function () {
+    console.log("... port %d in %s mode", 10000, "127.0.0.1");
   });
   console.log("ExpressJS Server Started.");
 };
